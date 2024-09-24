600f2e2f0a9c685e036897efd8a6b4520982c696
OfenbergerTobias <ofenberger.tobias@gmx.at>
2024-09-24 13:21:07 +0200

    add vcpkg and vscode build environment add abillity to debug main application add abillity to run/debug tests in vscode test explorer

---
+++ install_vcpkg.ps1
@@ -0,0 +1,22 @@
+# Set environment variables
+.\scripts\setEnvironmentVariables.ps1
+
+$mutex = New-Object System.Threading.Mutex($false, "Global\VCPKG_ACTIVATE_MUTEX")
+
+$vcpkg_activated = 0
+
+while ($vcpkg_activated -eq 0) {
+    if ($mutex.WaitOne(10000)) {
+        Invoke-Expression (Invoke-WebRequest -useb https://aka.ms/vcpkg-init.ps1)
+        vcpkg x-update-registry fronius
+        vcpkg activate        
+        vcpkg-shell activate
+        $mutex.ReleaseMutex()
+        $vcpkg_activated = 1
+    }
+}
+
+if ($args[0] -ne "novscode") {
+    code .
+}
+
