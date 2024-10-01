d55c120d1cf3a6e074e6f666cc64fce56a839a6a
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-10-01 09:57:07 +0200

    raw-mem-sensor: externalize get_temperature() in separate complation unit

--- toolcase/base/raw-mem-sensor.h
+++ toolcase/base/raw-mem-sensor.h
@@ -13,10 +13,7 @@ public:
         _mem = mem;
     }
 
-    double get_temperature()
-    {
-        return *((int32_t*)(_mem + RAW_MEM_SENSOR_TEMP_OFFSET)) / 1000.0d;
-    }
+    double get_temperature();
 
 private:
     char* _mem;
