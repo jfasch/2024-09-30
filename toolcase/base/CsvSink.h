b3299e356b1754829acf53b88878b2dea33efc6b
OfenbergerTobias <ofenberger.tobias@gmx.at>
2024-10-02 14:54:22 +0200

    create csv header

--- toolcase/base/CsvSink.h
+++ toolcase/base/CsvSink.h
@@ -9,13 +9,24 @@ class CsvSink : public IDataSink
 {
     public:
         CsvSink(const std::filesystem::path& path)
-        :_file(path)
-        {}
+        : CsvSink(path, !std::filesystem::exists(path))
+        {
+
+        }
         void write(double value) override
         {
             _file << value << std::endl;
         }
+
     private:
+        CsvSink(const std::filesystem::path& path, bool create)
+        : _file(path, create ? std::ios_base::trunc : std::ios_base::app)
+        {
+            if(create)
+            {
+                _file << "temperature" << ";" << std::endl;
+            }
+        }
 
         std::ofstream _file;
 };
