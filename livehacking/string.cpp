66426e7ab3a5190392bf3c8d72ddf547dcda6234
Joerg Faschingbauer <jf@faschingbauer.co.at>
2024-10-04 07:48:47 +0200

    string.cpp: prepare for "move" livehack

--- livehacking/string.cpp
+++ livehacking/string.cpp
@@ -5,7 +5,7 @@ class String
 {
 public:
     String()
-    : _s(new char[1]{})
+    : _s(nullptr)
     {}
     String(const char* s)
     : _s(new char[strlen(s)+1])
@@ -18,22 +18,30 @@ public:
     }
 
     String(const String& rhs)
-    : _s(new char[strlen(rhs._s)+1])
     {
-        strcpy(_s, rhs._s);
+        if (rhs._s == nullptr)
+            _s = nullptr;
+        else {
+            _s = new char[strlen(rhs._s)+1];
+            strcpy(_s, rhs._s);
+        }
     }
     String& operator=(const String& rhs)
     {
         if (this != &rhs) {                            // <-- self assignment check
             delete[] _s;
-            _s = new char[strlen(rhs._s)+1];
-            strcpy(_s, rhs._s);
+            _s = nullptr;
+
+            if (rhs._s != nullptr) {
+                _s = new char[strlen(rhs._s)+1];
+                strcpy(_s, rhs._s);
+            }
         }
         return *this;
     }
     
-    const char* c_str() const { return _s; }
-    size_t size() const { return strlen(_s); }
+    const char* c_str() const { return (_s == nullptr)? "\0": _s; }
+    size_t size() const { return (_s == nullptr)? 0: strlen(_s); }
 
 private:
     char* _s;
@@ -46,6 +54,11 @@ int main()
     s1 = s1;
 
     std::cout << s1.c_str() << std::endl;
+
+    String s2;
+    std::cout << s2.c_str() << std::endl;
+    s2 = s1;
+    std::cout << s2.c_str() << std::endl;
     
     return 0;
 }
